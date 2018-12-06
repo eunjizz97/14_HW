de23e3fb572a95b44f17a268991297275037527b
eunjizz97 <42995763+eunjizz97@users.noreply.github.com>
2018-12-06 22:07:36 +0900

    printAll
    노트북 인터넷이 안 돼서 다시 올립니다.

---
+++ linkedList.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct linknd {
+	int data;
+	
+	void *next;
+	//struct linked *next;
+} linknd_t;
+
+static linknd_t *list;
+
+
+linknd_t* create_node(int value) {
+	linknd_t* ndPtr;
+	
+	ndPtr = (linknd_t*) malloc( sizeof (linknd_t) );
+	
+	if(ndPtr == NULL)
+	{
+		printf("[error]\n");
+		return NULL;
+	}
+	
+	ndPtr->data = value;
+	ndPtr->next = NULL;
+	
+	return ndPtr;
+}
+
+void addTail(int value) {
+	linknd_t *ndPtr, *newPtr;
+	
+	if(list == NULL){
+		return;
+	}
+	else
+	{
+		ndPtr = list;
+		while(ndPtr->next != NULL){
+			ndPtr = ndPtr->next;
+		}
+		
+		newPtr = create_node(value);
+		ndPtr->next = newPtr;
+		
+	}
+}
+
+
+void genList(void) {
+	list = create_node(0);
+	
+	return;
+}
+
+
+void print_list(void) {
+	linknd_t *ndPtr = list;
+	
+	ndPtr = list->next;
+	while(ndPtr != NULL){
+		printf("%d ", ndPtr->data);
+		ndPtr = ndPtr->next;
+	}
+}
